b1fd837a8bec5e686d4e5ae2d07619e8a65db3bf
Ativelox <ativelox.dev@web.de>
2018-05-10 14:56:30 +0200

    Implemented a Screen Manager and related code closes #33 and #34

---
+++ Singularity/Singularity/property/IUpdate.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Singularity.property
+{
+    interface IUpdate
+    {
+        void Update(GameTime gametime);
+    }
+}
---
+++ Singularity/Singularity/screen/IScreenManager.cs
@@ -0,0 +1,18 @@
+﻿using Singularity.property;
+
+namespace Singularity.screen
+{
+    internal interface IScreenManager : IUpdate, IDraw
+    {   
+        /// <summary>
+        /// Adds the given screen to the screen manager as the "top" screen.
+        /// </summary>
+        /// <param name="screen">The new "top" screen</param>
+        void AddScreen(IScreen screen);
+
+        /// <summary>
+        /// Removes the "top" screen from the screen manager.
+        /// </summary>
+        void RemoveScreen();
+    }
+}
