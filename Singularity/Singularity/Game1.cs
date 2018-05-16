44bd1d83275d0db28da48369081b37c00f0c5d71
Ativelox <ativelox.dev@web.de>
2018-05-16 19:09:52 +0200

    Changed namspaces due to structure consistency.

--- Singularity/Singularity/Game1.cs
+++ Singularity/Singularity/Game1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Singularity.screen;
+using Singularity.Screen;
 
 namespace Singularity
 {
