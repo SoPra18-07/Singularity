d1f8b510375ad61c9409a2f9cf2b8fe44c7f4e95
eciaF <39056404+eciaF@users.noreply.github.com>
2018-05-28 12:56:01 +0200

    InputManager. closes #35, closes #36, closes #37

---
+++ Singularity/Singularity/Input/InputManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Singularity.Property;
+
+namespace Singularity.Input
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Manages the UserInput
+    /// </summary>
+    internal class InputManager : IUpdate
+    {
+        private readonly List<IKeyListener> mKeyListener;
+        private readonly List<IMouseListener> mMouseListeners;
+
+        public InputManager()
+        {
+            mKeyListener = new List<IKeyListener>();
+            mMouseListeners = new List<IMouseListener>();
+
+            var mouseEvent = new MouseEvent();
+            var keyEvent = new KeyEvent();
+        }
+
+        private void AddKeyListener(IKeyListener iKeyListener)
+        {
+            mKeyListener.Add(iKeyListener);
+        }
+
+        private bool RemoveKeyListener(IKeyListener iKeyListener)
+        {
+            if (!mKeyListener.Contains(iKeyListener))
+            {
+                return false;
+            }
+
+            mKeyListener.Remove(iKeyListener);
+            return true;
+        }
+
+        private void AddMouseListener(IMouseListener iMouseListener)
+        {
+            mMouseListeners.Add(iMouseListener);
+        }
+
+        private bool RemoveMouseListener(IMouseListener iMouseListener)
+        {
+            if (!mMouseListeners.Contains(iMouseListener))
+            {
+                return false;
+            }
+
+            mMouseListeners.Remove(iMouseListener);
+            return true;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Singularity/Singularity/Input/MouseEvent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Singularity.Input
+{
+    internal sealed class MouseEvent
+    {
+        public Vector2 Position { get; } = Vector2.One;
+
+        public EMouseAction MouseAction { get; } = EMouseAction.LeftClick; // solve warnings for now
+
+
+        public enum EMouseAction
+        {
+            LeftClick,
+            RightClick
+        };
+    }
+}
