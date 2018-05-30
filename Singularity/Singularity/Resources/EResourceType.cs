67659e95899857dc6076bd20a835f48a24124029
Yvan Satyawan <y_satyawan@hotmail.com>
2018-05-30 14:16:56 +0200

    Update EResourceType.cs
    Change none to trash and improved documentation

--- Singularity/Singularity/Resources/EResourceType.cs
+++ Singularity/Singularity/Resources/EResourceType.cs
@@ -11,68 +11,70 @@
     internal enum EResourceType
     {
         /// <summary>
-        /// This represents the water resource.
+        /// This represents the water resource and is a raw resource.
         /// </summary>
         Water,
 
         /// <summary>
-        /// This represents the sand resource.
+        /// This represents the sand resource and is a raw resource.
         /// </summary>
         Sand,
 
         /// <summary>
-        /// This represents the oil resource.
+        /// This represents the oil resource and is a raw resource.
         /// </summary>
         Oil,
 
         /// <summary>
-        /// This represents the metal resource.
+        /// This represents the metal resource and is a raw resource.
         /// </summary>
         Metal,
 
         /// <summary>
-        /// This represents the stone resource.
+        /// This represents the stone resource and is a raw resource.
         /// </summary>
         Stone,
 
         /// <summary>
-        /// This represents the concrete resource.
+        /// This represents the concrete resource and is made from sand and water.
         /// </summary>
         Concrete,
 
         /// <summary>
-        /// This represents the silicon resource.
+        /// This represents the silicon resource and is made from sand.
         /// </summary>
         Silicon,
 
         /// <summary>
-        /// This represents the plastic resource.
+        /// This represents the plastic resource and is made from oil.
         /// </summary>
         Plastic,
 
         /// <summary>
-        /// This represents the fuel resource.
+        /// This represents the fuel resource and is made from oil.
         /// </summary>
         Fuel,
 
         /// <summary>
-        /// This represents the steel resource.
+        /// This represents the steel resource and is made from metal.
         /// </summary>
         Steel,
 
         /// <summary>
-        /// This represents the copper resource.
+        /// This represents the copper resource and is made from metal.
         /// </summary>
         Copper,
 
         /// <summary>
-        /// This represents the chip resource.
+        /// This represents the chip resource and is made from copper, silicon,
+        /// and plastic.
         /// </summary>
         Chip,
 
         /// <summary>
-        /// This represents an empty resource.
+        /// This represents a trash resource. This resource appears at a fixed rate
+        /// when other resources are process by factories.
         /// </summary>
-        None
+        Trash
     };
 }
