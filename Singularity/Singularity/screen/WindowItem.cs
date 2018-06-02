8e7420a03970d1f69c671d5977f3fbdee36a1e42
N <hufflepuff79@users.noreply.github.com>
2018-06-02 13:24:37 +0200

    interface and class for window iterms

---
+++ Singularity/Singularity/screen/WindowItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Singularity.Units
+{
+    interface WindowItem
+    {
+        void Draw(SpriteBatch spriteBatch);
+
+        void Update(GameTime gameTime);
+
+        // TODO void ReceiveEvents(Event e)
+
+    }
+}
