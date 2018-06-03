23396d85fc59aa0e6a42b86dc870f5b1bebe0bda
Yvan Satyawan <y_satyawan@hotmail.com>
2018-06-03 16:45:36 +0200

    MenuBackgroundScreen updated

--- Singularity/Singularity/screen/ScreenClasses/SplashScreen.cs
+++ Singularity/Singularity/screen/ScreenClasses/SplashScreen.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Singularity.Screen
@@ -17,6 +18,11 @@ namespace Singularity.Screen
     /// </summary>
     class SplashScreen : IScreen
     {
+
+        public void LoadContent(ContentManager content)
+        {
+            content.Load<Texture2D>()
+        }
         public void Update(GameTime gametime)
         {
             throw new NotImplementedException();
