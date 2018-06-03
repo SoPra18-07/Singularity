d86fa8aca9f50d2f7307fe201657825f43a1dc98
Confectio <leonard.fichtner@web.de>
2018-06-03 21:21:38 +0200

    Copied the code as in the other branches before. Going to add the specific methods now.

---
+++ Singularity/Singularity/platform/Junkyard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Singularity.Resources;
+
+namespace Singularity.platform
+{
+    [DataContract()]
+    class Junkyard : PlatformBlank
+    {
+        public Junkyard(Vector2 position, Texture2D spritesheet): base(position, spritesheet)
+        {
+            //mActions = IPlatformAction[];
+            //mActions[0] = ProduceQuarryResource(this);
+            //mActions[1] = ;
+            //Add Costs of the platform here if you got them.
+            mCost = new Dictionary<IResources, int>();
+        }
+
+        public override void Produce()
+        {
+            throw new NotImplementedException();
+            //foreach (var pair in mAssignedUnits)
+            //{
+            //if (pair.Value.GetJobType() == JobType.Production)
+            //{
+            //pair.Key.Produce();
+            //}
+
+            //}
+        }
+    }
+}
