f440ec8b5247fdb8a38a6cb66d4ed13f2c1da4db
Confectio <leonard.fichtner@web.de>
2018-06-03 21:01:28 +0200

    Basically copied my code from Well.cs with minor changes.

---
+++ Singularity/Singularity/platform/Quarry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Singularity.Resources;
+
+namespace Singularity.platform
+{
+    class Quarry : PlatformBlank
+    {
+        public Quarry(Vector2 position, Texture2D spritesheet): base(position, spritesheet)
+        {
+            //mActions = IPlatformAction[];
+            //mActions[0] = ProduceQuarryResource(this);
+            //mActions[1] = ;
+            //Add Costs of the platform here if you got them.
+            mCost = new Dictionary<IResources, int>();
+        }
+
+        public override void Produce()
+        {
+            throw new NotImplementedException();
+            //foreach (var pair in mAssignedUnits)
+            //{
+            //if (pair.Value.GetJobType() == JobType.Production)
+            //{
+            //pair.Key.Produce();
+            //}
+
+            //}
+        }
+    }
+}
