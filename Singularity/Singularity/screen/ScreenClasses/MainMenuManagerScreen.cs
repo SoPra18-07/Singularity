703d5d2808ab8168d1938b81884710a6f31eeeb7
Yvan Satyawan <y_satyawan@hotmail.com>
2018-06-04 17:39:57 +0200

    Added documentation for MainMenuManagerScreen and renamed windowitem.cs
    Changed file name to correspond to the fact that it is an interface

--- Singularity/Singularity/screen/ScreenClasses/MainMenuManagerScreen.cs
+++ Singularity/Singularity/screen/ScreenClasses/MainMenuManagerScreen.cs
@@ -1,62 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Singularity.Libraries;
 
 namespace Singularity.Screen.ScreenClasses
 {
     internal sealed class MainMenuManagerScreen : IScreen
     {
-        private IScreenManager mScreenManager;
-        private static Vector2 sMenuBox;
+        /// <inheritdoc cref="IScreen"/>
+        /// <summary>
+        /// Handles everything thats going on explicitly in the game.
+        /// E.g. game objects, the map, camera. etc.
+        /// </summary>
+        private readonly IScreenManager mScreenManager;
         private EScreen mScreenState;
 
-        // Fonts
-        private SpriteFont mLibSans36;
-        private SpriteFont mLibSans20;
-
         // All connecting screens
-        private IScreen mGameModeSelectScreen;
-        private IScreen mLoadSelectScreen;
-        private IScreen mAchievementsScreen;
-        private IScreen mOptionsScreen;
-        private IScreen mSplashScreen;
-        private IScreen mMainMenuScreen;
+        private readonly IScreen mGameModeSelectScreen;
+        private readonly IScreen mLoadSelectScreen;
+        private readonly IScreen mAchievementsScreen;
+        private readonly IScreen mOptionsScreen;
+        private readonly IScreen mSplashScreen;
+        private readonly IScreen mMainMenuScreen;
 
         // Background
-        private MenuBackgroundScreen mMenuBackgroundScreen;
-
-        // all text is stored as string variables to allow for easy changes
-        private readonly string mPlayString;
-        private readonly string mLoadSelectString;
-        private readonly string mAchievementsString;
-        private readonly string mOptionsString;
-        private readonly string mQuitString;
-        private readonly string mTitle;
-
-        // Buttons on the main menu
-        private Button mPlayButton;
-        private Button mLoadButton;
-        private Button mAchievementsButton;
-        private Button mOptionsButton;
-        private Button mQuitButton;
+        private readonly MenuBackgroundScreen mMenuBackgroundScreen;
 
         /// <summary>
-        /// Shows the main menu screen with 5 options:
-        /// New Game, Load Game, Achievements, Options, and Quit Game.
+        /// Manages the main menu. This is the screen that is first loaded into the stack screen manager
+        /// and loads all the other main menu screens. Also handles button events which involve switching
+        /// between menu screens.
         /// </summary>
-        /// <param name="screenResolution">Screen resolution of the game</param>
-        /// <param name="screenManager">Stack screen manager of the game</param>
-        /// <param name="showSplash">Defines if the splash screen should be shown</param>
+        /// <param name="screenResolution">Screen resolution of the game.</param>
+        /// <param name="screenManager">Stack screen manager of the game.</param>
+        /// <param name="showSplash">Defines if the splash screen should be shown
+        /// (used when going back to main menu from within the game where showing the
+        /// splash screen would not be necessary).</param>
         public MainMenuManagerScreen(Vector2 screenResolution, IScreenManager screenManager, bool showSplash)
         {
-            SetResolution(screenResolution);
             mScreenManager = screenManager;
 
             mGameModeSelectScreen = new GameModeSelectScreen();
@@ -68,14 +50,6 @@ namespace Singularity.Screen.ScreenClasses
             mMainMenuScreen = new MainMenuScreen(screenResolution);
 
             mScreenState = showSplash ? EScreen.SplashScreen : EScreen.MainMenuScreen;
-        }
-
-        public static void SetResolution(Vector2 screenResolution)
-        {
-            sMenuBox = new Vector2(screenResolution.X / 2 - 150, screenResolution.Y / 2 - 175);
-
-            // TODO change size of menu box based on the resolution of the screen 
-
         }
 
         public void LoadContent(ContentManager content)
@@ -109,7 +83,7 @@ namespace Singularity.Screen.ScreenClasses
 
         public void Update(GameTime gametime)
         {
-            // TODO this is temporary. This will be replaced with event handlers from the buttons
+            // TODO this is temporary. This will be replaced with event handlers from the buttons or input
             switch (mScreenState)
             {
                 case EScreen.AchievementsScreen:
