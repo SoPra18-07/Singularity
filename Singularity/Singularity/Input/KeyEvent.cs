e93147d33b3090bb10046e2f72cd7ff8b267c79c
eciaF <39056404+eciaF@users.noreply.github.com>
2018-06-04 23:16:40 +0200

    fixed the InputManager. closes #115
    The InputManager should be ready for use

--- Singularity/Singularity/Input/KeyEvent.cs
+++ Singularity/Singularity/Input/KeyEvent.cs
@@ -1,15 +1,14 @@
-﻿
-using Microsoft.Xna.Framework.Input;
+﻿using Microsoft.Xna.Framework.Input;
 
 namespace Singularity.Input
 {
-    internal sealed class KeyEvent
+    public sealed class KeyEvent
     {
         public KeyEvent(Keys[] currentKeys)
         {
             CurrentKeys = currentKeys;
         }
 
-        private Keys[] CurrentKeys { get; }
+        public Keys[] CurrentKeys { get; }
     }
 }
