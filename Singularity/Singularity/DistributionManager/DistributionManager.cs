2012c7792fc454a1c64e088e3cd862de921ac759
Confectio <leonard.fichtner@web.de>
2018-06-11 10:58:28 +0200

    Creating Files

---
+++ Singularity/Singularity/DistributionManager/DistributionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Singularity.Units;
+
+namespace Singularity.DistributionManager
+{
+    [DataContract()]
+    class DistributionManager
+    {
+        [DataMember()]
+        private List<GeneralUnit> mUnits;
+    }
+}
