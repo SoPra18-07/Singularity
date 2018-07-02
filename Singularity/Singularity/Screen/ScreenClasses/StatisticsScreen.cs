3566fbbda07fac6a7f972c0c29d20f1ea4b27484
balazsn <39065979+Akai-Ryu@users.noreply.github.com>
2018-07-02 13:55:00 +0200

    Add files via upload

--- Singularity/Singularity/Screen/ScreenClasses/StatisticsScreen.cs
+++ Singularity/Singularity/Screen/ScreenClasses/StatisticsScreen.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Singularity.Libraries;
+using Singularity.Manager;
 
 namespace Singularity.Screen.ScreenClasses
 {
@@ -13,29 +16,43 @@ namespace Singularity.Screen.ScreenClasses
     /// filter different statistics.
     /// </summary>
 
-    class Statistics : IScreen
+    class StatisticsScreen : IScreen
     {
-        public EScreen Screen { get; private set; } = EScreen.StatisticsScreen;
+        // Fonts
+        private SpriteFont mLibSans20;
+        private SpriteFont mLibSans14;
 
-        public bool Loaded { get; set; }
+        // Buttons
+        private Button mCloseButton;
+
+        // Backup of StatisticsScreen position to update button positions.
+        private Vector2 mPrevPosition;
+
+        // ScreenManager to add / close screens.
+        private readonly IScreenManager mScreenManager;
+
+        // Director to gain access to StoryManager.
+        private readonly Director mDirector;
+
+
+        public StatisticsScreen(Vector2 screenSize, IScreenManager screenManager, Director director)
+        {
+            mScreenManager = screenManager;
+            mDirector = director;
+
+            Position = new Vector2(screenSize.X / 2 - 205, screenSize.Y / 2 - 225);
+            mPrevPosition = Position;
+
+        }
 
         /// <summary>
         /// Updates the contents of the screen.
         /// </summary>
         /// <param name="gametime">Current gametime. Used for actions
-        /// that take place over time.</param>
+        /// that take place over time </param>
         public void Update(GameTime gametime)
         {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Draws the content of this screen.
-        /// </summary>
-        /// <param name="spriteBatch">spriteBatch that this object should draw to.</param>
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            throw new NotImplementedException();
+            mCloseButton.Update(gametime);
         }
 
         /// <summary>
@@ -44,8 +61,47 @@ namespace Singularity.Screen.ScreenClasses
         /// <param name="content">Content Manager that should handle the content loading</param>
         public void LoadContent(ContentManager content)
         {
-            throw new NotImplementedException();
+            mLibSans20 = content.Load<SpriteFont>("LibSans20");
+            mLibSans14 = content.Load<SpriteFont>("LibSans14");
+
+            mCloseButton = new Button("Close", mLibSans20, new Vector2(Position.X + 167, Position.Y + 410)) { Opacity = 1f };
+
+            mCloseButton.ButtonReleased += CloseButtonReleased;
         }
+
+        /// <summary>
+        /// Draws the content of this screen.
+        /// </summary>
+        /// <param name="spriteBatch">spriteBatch that this object should draw to.</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.FillRectangle(Position, new Vector2(400, 450), new Color(0.27f, 0.5f, 0.7f, 1f), 0f);
+            mCloseButton.Draw(spriteBatch);
+            // Draw the window name and stats names.
+            spriteBatch.DrawString(mLibSans20, "Statistics", new Vector2(Position.X + 145, Position.Y + 10), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Time:", new Vector2(Position.X + 60, Position.Y + 60), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Units created:", new Vector2(Position.X + 60, Position.Y + 110), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Units lost:", new Vector2(Position.X + 60, Position.Y + 150), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Units killed:", new Vector2(Position.X + 60, Position.Y + 190), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Ressources created:", new Vector2(Position.X + 60, Position.Y + 240), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Platforms created:", new Vector2(Position.X + 60, Position.Y + 290), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Platforms lost:", new Vector2(Position.X + 60, Position.Y + 370), Color.Black);
+            spriteBatch.DrawString(mLibSans14, "Platforms destroyed:", new Vector2(Position.X + 60, Position.Y + 330), Color.Black);
+            // Draw the stats behind their names, live from the StoryManager.
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.Time.ToString(), new Vector2(Position.X + 110, Position.Y + 60), Color.Black);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mUnits["created"].ToString(), new Vector2(Position.X + 180, Position.Y + 110), Color.LawnGreen);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mUnits["lost"].ToString(), new Vector2(Position.X + 180, Position.Y + 150), Color.Red);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mUnits["killed"].ToString(), new Vector2(Position.X + 180, Position.Y + 190), Color.Black);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mResources.Sum(x => x.Value).ToString(), new Vector2(Position.X + 245, Position.Y + 240), Color.LawnGreen);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mPlatforms["created"].ToString(), new Vector2(Position.X + 240, Position.Y + 290), Color.LawnGreen);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mPlatforms["lost"].ToString(), new Vector2(Position.X + 240, Position.Y + 370), Color.Red);
+            spriteBatch.DrawString(mLibSans14, mDirector.GetStoryManager.mPlatforms["destroyed"].ToString(), new Vector2(Position.X + 240, Position.Y + 330), Color.Black);
+
+            spriteBatch.End();
+        }
+
 
         /// <summary>
         /// Determines whether or not the screen below this on the stack should update.
@@ -53,7 +109,7 @@ namespace Singularity.Screen.ScreenClasses
         /// <returns>Bool. If true, then the screen below this will be updated.</returns>
         public bool UpdateLower()
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         /// <summary>
@@ -62,7 +118,19 @@ namespace Singularity.Screen.ScreenClasses
         /// <returns>Bool. If true, then the screen below this will be drawn.</returns>
         public bool DrawLower()
         {
-            throw new NotImplementedException();
+            return true;
         }
+
+        private void CloseButtonReleased(object sender, EventArgs eventArgs)
+        {
+            mScreenManager.RemoveScreen();
+        }
+
+        public EScreen Screen { get; private set; } = EScreen.StatisticsScreen;
+
+        public bool Loaded { get; set; }
+
+        // TODO : USE MEMBER VARIABLE INSTEAD?
+        private Vector2 Position { get; }
     }
 }
