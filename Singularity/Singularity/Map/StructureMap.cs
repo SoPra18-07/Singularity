e762fca8646a733671351d164e1da190c54a1b77
Ativelox <ativelox.dev@web.de>
2018-07-08 21:50:37 +0900

    Removed debug prints

--- Singularity/Singularity/Map/StructureMap.cs
+++ Singularity/Singularity/Map/StructureMap.cs
@@ -499,7 +499,6 @@ namespace Singularity.Map
                 mGraphIdToEnergyLevel[graphId] = mGraphIdToEnergyLevel[graphId] + ((PlatformBlank) node).GetProvidingEnergy();
                 mGraphIdToEnergyLevel[graphId] = mGraphIdToEnergyLevel[graphId] - ((PlatformBlank) node).GetDrainingEnergy();
             }
-            Debug.WriteLine(mGraphIdToEnergyLevel[graphId]);
 
             CheckEnergyLevel(graphId, wasNegative);
         }
