f27db0edcf66c7eade00b5294adae9d19af7d9db
eciaF <39056404+eciaF@users.noreply.github.com>
2018-07-18 01:03:13 +0200

    quick resharper

--- Singularity/Singularity/Screen/ScreenClasses/LoseScreen.cs
+++ Singularity/Singularity/Screen/ScreenClasses/LoseScreen.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Singularity.Libraries;
 using Singularity.Manager;
-using Singularity.Units;
 
 namespace Singularity.Screen.ScreenClasses
 {
