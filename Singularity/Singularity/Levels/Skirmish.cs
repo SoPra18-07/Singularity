441521b9c799bdef23618d15146c15264d66af60
Ativelox <ativelox.dev@web.de>
2018-07-17 22:40:24 +0900

    Made the game loseable

--- Singularity/Singularity/Levels/Skirmish.cs
+++ Singularity/Singularity/Levels/Skirmish.cs
@@ -36,10 +36,7 @@ namespace Singularity.Levels
             GameScreen.AddObject(new Puddle(new Vector2(3300, 2500), ref mDirector));
             GameScreen.AddObject(new Puddle(new Vector2(3300, 2700), ref mDirector, false));
 
-            for (int i = 0; i < 100; i++)
-            {
-                GameScreen.AddObject(new MilitaryFast(new Vector2(3000, 3000), Camera, ref mDirector, ref map));
-            }
+            GameScreen.AddObject(new EnemyFast(new Vector2(3000, 3500), Camera, ref mDirector, ref map));
 
             //TESTMETHODS HERE ====================================
         }
