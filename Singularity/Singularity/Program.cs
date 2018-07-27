ba2d578d090387141517bff3f0dd8d97fcca94a9
fkarg <f.karg10@gmail.com>
2018-07-27 17:30:43 +0200

    Revert "Revert "Revert "now builds on linux. Broke FoW for some reason"""
    This reverts commit f6892189a6e466e40853f27be76758e0d1152f9f.

--- Singularity/Singularity/Program.cs
+++ Singularity/Singularity/Program.cs
@@ -14,7 +14,9 @@ namespace Singularity
         static void Main()
         {
             using (var game = new Game1())
+            {
                 game.Run();
+            }
         }
     }
 }
